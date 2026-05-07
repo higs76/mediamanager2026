1b60a7802e7d7c9b59ce2ada1d70826e34eb765d
higs76 <dvincent76@hotmail.com>
2026-05-07 11:10:37 +0200

    Enhance container ID selection logic
    Add validation for container ID input and handle 'auto' option.

--- scripts/mediamanager-proxmox.sh
+++ scripts/mediamanager-proxmox.sh
@@ -42,7 +42,22 @@ log_success "Running on Proxmox VE"
 
 #check container ID
 log_info "Finding next available Container ID..."
-CTID=$(pvesh get /cluster/nextid)
+if [ "$CTID" = "auto" ]; then
+    CTID=$(pvesh get /cluster/nextid)    
+else
+    # Tant que l'ID est déjà utilisé OU n'est pas un nombre
+    while [[ ! "$CTID" =~ ^[0-9]+$ ]] || pct list | awk '{print $1}' | grep -qW "$CTID"; do
+    echo "L'ID '$CTID' est invalide ou déjà utilisé."
+        read -p "Veuillez saisir un autre ID (ou 'auto') : " CTID
+
+        if [ "$CTID" = "auto" ]; then
+            CTID=$(pvesh get /cluster/nextid)
+            break # On sort de la boucle puisqu'on a un ID valide
+        fi
+fi
+
+
+
 
 # Obtenir le prochain CTID disponible (LXC ou VM)
 
